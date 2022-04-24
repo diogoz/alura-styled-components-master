b368294181f4152750e0f0c949785c6dfc300c47
Diogo <dhesousa77@gmail.com>
2022-04-24 17:44:20 -0300

    feat: adicionado componente extrato

--- src/Components/Extrato/index.jsx
+++ src/Components/Extrato/index.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Box, Botao } from "../UI";
 import { extratoLista } from "../../info";
+import Items from "../Items";
 
 export const Extrato = () => {
   return (
     <Box>
       {extratoLista.updates.map(({ id, type, from, value, date }) => {
         return (
-          <div key={id}>
-            <div>{type}</div>
-            <div>{from}</div>
-          </div>
+          <Items key={id} from={from} type={type} value={value} date={date} />
         );
       })}
       <Botao>Ver mais</Botao>
